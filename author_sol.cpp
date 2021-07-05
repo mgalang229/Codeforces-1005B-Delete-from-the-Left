c6f77c9e7e955a26d4c8767192b02cf955e1096a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-07-05 13:01:32 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	string s, t;
+	cin >> s >> t;
+	int w = 0;
+	// start comparing letters from the both of their last characters
+	while (true) {
+		// set the last index for the string 's'
+		int i = s.length() - w - 1;
+		// set the last index for the string 't'
+		int j = t.length() - w - 1;
+		if (i >= 0 && j >= 0 && s[i] == t[j]) {
+			// if the characters match, then increment 'w'
+			w++;
+		} else {
+			// otherwise, break the loop
+			break;
+		}
+	}
+	// get the difference of the combined total length of the two strings and the number
+	// of matched characters times two (since there is only one operation per step)
+	cout << s.length() + t.length() - 2 * w << '\n';
+	return 0;
+}
